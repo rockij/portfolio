c82bc423140aeb0cc6a2b3b8ba8fcf25fa3ef3bc
rockij83 <rockij83@nate.com>
2021-06-06 22:53:10 +0900

    210606

---
+++ blog/worksource/scroll-parallax/simple-scrollspy/webpack.config.js
@@ -0,0 +1,18 @@
+const { resolve } = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+    entry: resolve(__dirname, 'src/index.js'),
+
+    output: {
+        path: resolve(__dirname, 'demo/dist'),
+        filename: 'simple-scrollspy.min.js',
+        library: 'scrollSpy',
+        libraryTarget: 'umd'
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+}
