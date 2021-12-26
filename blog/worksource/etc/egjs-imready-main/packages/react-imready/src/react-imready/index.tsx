41650dc5d4f4cc0baf96eb538d51ef433cc31186
kimikjoong <rockij83@nate.com>
2021-12-27 00:06:52 +0900

    211227

--- blog/worksource/etc/egjs-imready-main/packages/react-imready/src/react-imready/index.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * @namespace ReactImReady
- */
-export * from "./useImReady";
-export * from "./usePreReadyElement";
-export * from "./useReadyElement";
-export * from "./usePreReady";
-export * from "./useReady";
-export * from "./types";
--- blog/worksource/etc/egjs-imready-main/packages/vue-imready/src/vue-imready/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @namespace VueImReady
- */
-export * from "./useImReady";
-export * from "./usePreReadyElement";
-export * from "./useReadyElement";
-export * from "./usePreReady";
-export * from "./useReady";
-export * from "./types";
