f61cdfd097d2357bf7f971d8a47a4bfb3cf3286d
김익중 <rockij83@nate.com>
2023-04-11 22:30:26 +0900

    230411

---
+++ resource/js/items.js
@@ -0,0 +1,45 @@
+let items = [
+  {
+    productName: "KB Wallet 구축(vue)",
+    category: 'mobile',
+    workdate: '22.03~23.03(12개월)',
+    url: '',
+  },
+  {
+    productName: "KB liivemate 마이데이터 구축 앱 고도화",
+    category: 'mobile',
+    workdate: '21.04~21.12(8개월)',
+    url: '',
+  },
+  {
+    productName: "KB 기업뱅킹 리뉴얼",
+    category: 'mobile',
+    workdate: '21.01~21.03(3개월)',
+    url: '',
+  },
+  {
+    productName: 'PASS앱 구축 및 운영',
+    category: 'mobile',
+    workdate: '-',
+    url: 'https://www.sktpass.com/',
+  },
+  {
+    productName: '티오르 구축',
+    category: 'mobile',
+    workdate: '-',
+    url: 'https://tior.co.kr/',
+  },
+  {
+    productName: '주식투자노트 구축 및 운영',
+    category: 'mobile',
+    category2: 'pc',
+    workdate: '-',
+    url: 'https://stockn.kr/',
+  },
+  // {
+  //   productName: '',
+  //   category: '',
+  //   workdate: '-',
+  //   url: 'html/.html',
+  // },
+]
